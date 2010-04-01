10ecc323e06729eca159bee22decec2f2e2cc3be
trapd00r <trapd00r@trapd00r.se>
2010-04-01 10:00:19 +0200

    added some

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,3 +1,4 @@
+# vim: textwidth=0 nowrap ts=2
 alias -s {pl,pm,c,cpp,h,sh}='vim'
 alias -s {mkv,avi}='mplayer'
 alias -s PKGBUILD='vim'
@@ -15,9 +16,9 @@ alias make='make -j3'
 alias :q='exit'
 alias cat='vimpager'
 
-
 alias c256='printf "\033[38;5;11m foobar"'
 # shortcuts
+alias a='cd $HOME/devel/trunk'
 alias b='cd $HOME/bin'
 alias c='cd $HOME/configs'
 alias d='cd $HOME/devel'
@@ -61,9 +62,10 @@ alias g++='$HOME/bin/cope/g++'
 alias ifconfig='$HOME/bin/cope/ifconfig'
 alias netstat='$HOME/bin/cope/netstat'
 alias nmap='$HOME/bin/cope/nmap'
+alias ps='$HOME/bin/cope/ps'
 
 # file management
-alias ls='\ls -hovA --indicator-style=file-type \
+alias ls='$HOME/bin/cope/ls -hovA --indicator-style=file-type \
           --color=always --group-directories-first --time=ctime \
           --time-style=full-iso'
 alias ls1='\ls -1 --color=auto --time-style=full-iso'
@@ -73,37 +75,38 @@ alias lsl='ls -L --time-style=full-iso'
 alias lss="\ls -1 --color=auto|grep '(^\w*[^_-])'"
 alias lsq='ls -AlQ --color=always --time-style=full-iso'
 
+# application sane defaults
+alias iostat='iostat -mtx'
+alias mplayer='mplayer -slave -input file=$HOME/.mplayer/fifo'
+alias dmesg='dmesg|matchline -dmesg'
+alias lsusb='lsusb|matchline -random'
+alias lspci='lspci|matchline -lspci'
+alias feh='feh -Fzrd'
+alias memusage='ps -e -orss=,args= | sort -b -k1,1n|pr -TW$COLUMNS'
+alias pacman='pacman-color'
+alias scp='scp -P 19216'
+alias urxvt='urxvt -name URxvt.shiva'
+alias tree='tree -dA'
 
-# random
-alias mplayer='mplayer -slave -input file=/home/scp1/.mplayer/fifo'
+# other useful stuff
+alias shiva="ps -eo pcpu,pid,user,args|sort -k 1 -r|head -10 && iostat -mtx && mpstat -P 'ALL' && sar"
+alias cpuu='ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10'
+alias ss='source $HOME/.zshrc'
+alias ssa='mpc search artist'
+alias paste='curlpaste -l perl -s codepad -X'
 alias s='stumpish restore-from-file code'
 alias v+='ossmix vmix0.pcm8 -- +2'
 alias v-='ossmix vmix0.pcm8 -- -2'
-alias rec='ffmpeg -f x11grab -s 1680x1050 -r 120 -i :0.0 -sameq foo.mpg'
-alias dmesg='dmesg|matchline -dmesg'
-alias dvdc='ssh 192.168.1.33 -p 19216'
-alias feh='feh -FZrd'
+alias rec='ffmpeg -f x11grab -s 3360x1050 -r 150 -i :0.0 -sameq /tmp/foo.mpg'
+alias dvdc='ssh scp1@192.168.1.33 -p 19216'
 alias fevil='find . -regextype posix-extended -regex'
-alias lspci='lspci|matchline -lspci'
-alias lsusb='lsusb|matchline -random'
-alias memusage='ps -e -orss=,args= | sort -b -k1,1n | pr -TW$COLUMNS'
-#alias mntmp3="mount /dev/$(dmesg|\tail -1|awk {'print $3'}| \
 alias mountfixbug='echo 64 > /sys/block/sdn/device/max_sectors && mount -a'
 alias np='pimpd -i'
-alias ob='startx /usr/bin/openbox-session -- /usr/bin/Xnest :2'
-alias pacman='pacman-color'
-alias pacrem='pacman -Rs $(pacman -Qtdq)'
-alias pasta='wgetpaste -l Perl -X -s ca'
 alias pdb='perl -d -e 1'
-alias ps='/home/scp1/bin/cope/ps'
 alias qi='qemu -cdrom iso -boot d hd'
-alias rmvi='rm *.swp'
+alias rmvi='rm *.sw*'
 alias R='rehash'
-alias scp='scp -P 19216'
+alias reset='printf "\033c\033(K\033[J\033[0m\033[?25h"'
 alias scx='screen -x'
 alias share='python -c "import SimpleHTTPServer;SimpleHTTPServer.test()"'
 alias sortbycolumn='sort -n -k3'
-alias tree='tree -dA'
-alias urxvt='urxvt -name URxvt.shiva'
-#alias vim='echo -en "\033]50;-*-terminus-*-*-*-*-14-*-72-72-*-*-*-*\007" && vim'
-alias wgeturlfromfile='wget -r -l1 -H -t1 -nd -N -np -A.jpg -erobots=off -i' 
