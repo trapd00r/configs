2757cdbcde1fa3d8d7a8709ed11e1c57fb381cdf
Magnus Woldrich <trapd00r@trapd00r.se>
2011-02-01 20:57:16 +0100

    add compile flags for mpd

---
+++ Compile_Flags/mpd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# MPD
+sh configure --enable-cue --enable-httpd-output --enable-gme --disable-inotify \
+--disable-ipv6 --enable-lame-encoder --enable-mikmod --enable-modplug \
+--enable-mpg123 --enable-sidplay --enable-vorbis-encoder
+
+
+# vim: set ts=2 expandtab sw=2:
+
+
