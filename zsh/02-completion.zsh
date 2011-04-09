7a1205eb4e07799afccb79d77968c70cdf84f700
Magnus Woldrich <m@japh.se>
2011-04-09 17:52:41 +0200

    zsh: more completion goodness; vim!

--- zsh/02-completion.zsh
+++ zsh/02-completion.zsh
@@ -1,29 +1,46 @@
 #!/usr/bin/zsh
 
-zstyle ':completion:*'                    list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*:default'            list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*'                    menu select=10
-zstyle ':completion:*'                    muttrc='$HOME/configs/.muttrc'
-zstyle ':completion:*'                    use-perl=1
-zstyle ':completion:*'                    my-accounts='m@japh.se'
+zstyle ':completion:*'                       list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:default'               list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*'                       menu select=10
+zstyle ':completion:*'                       muttrc='$HOME/configs/.muttrc'
+zstyle ':completion:*'                       use-perl=1
+zstyle ':completion:*'                       my-accounts='m@japh.se'
 
-zstyle ':completion:*'                    squeeze-slashes true
-zstyle ':completion:*:cd:*'               ignore-parents parent pwd
+zstyle ':completion:*'                       squeeze-slashes true
+zstyle ':completion:*:cd:*'                  ignore-parents parent pwd
 
-zstyle ':completion:*:(all-|)files'       ignored-patterns '*.un~'
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:(all-|)files'          ignored-patterns '*.un~'
+zstyle ':completion:*:*:kill:*:processes'    list-colors '=(#b) #([0-9]#)*=0=01;31'
 
-zstyle ':completion::complete:*'          use-cache on
-zstyle ':completion::complete:*'          cache-path ~/.zsh/cache/$HOST
-zstyle ':completion:*:processes'          command 'ps -axw'
-zstyle ':completion:*:processes-names'    command 'ps -awxho command'
-zstyle ':completion:*'                    matcher-list 'm:{a-z}={A-Z}'
-zstyle ':completion:*:functions'          ignored-patterns '_*'
-zstyle ':completion:*:scp:*'              tag-order \
-zstyle ':completion:*:scp:*'              group-order \
-zstyle ':completion:*:ssh:*'              tag-order \
-zstyle ':completion:*:ssh:*'              group-order \
-zstyle ':completion:*:*:mplayer:*'        tag-order files directories
+zstyle ':completion::complete:*'             use-cache on
+zstyle ':completion::complete:*'             cache-path ~/.zsh/cache/$HOST
+zstyle ':completion:*:processes'             command 'ps -axw'
+zstyle ':completion:*:processes-names'       command 'ps -awxho command'
+zstyle ':completion:*'                       matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*:functions'             ignored-patterns '_*'
+zstyle ':completion:*:scp:*'                 tag-order \
+zstyle ':completion:*:scp:*'                 group-order \
+zstyle ':completion:*:ssh:*'                 tag-order \
+zstyle ':completion:*:ssh:*'                 group-order \
+zstyle ':completion:*:*:mplayer:*'           tag-order files directories
+zstyle ':completion:*:*:(gvim|vim):*:*files' ignored-patterns '*~' file-sort modification
+zstyle ':completion:*:*:(gvim|vim):*'        file-sort modification
+zstyle ':completion:*:descriptions'          format $'%{\e[38;5;070;1m\e[48;5;234m%}%B%d%b%{\e[m%}'
+zstyle ':completion:*:warnings'              format $'%{\e[38;5;196;1m%}Nah: %d%{\e[m%}'
+
+zstyle ':completion:*:*:*:users' ignored-patterns \
+        adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
+        dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
+        hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
+        mailman mailnull mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
+        operator pcap postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs
+zstyle '*' single-ignored show
+
+zstyle ':completion:*:options'               menu search 
+
 
 zstyle ':completion:*:(ssh|scp):*:my-accounts' users-hosts \
   'scp1@192.168.1.100' 'scp1@brutus.ethup.se' 'trapd00r@90.225.22.81'
