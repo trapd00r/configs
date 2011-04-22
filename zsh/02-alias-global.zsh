7d088aeb96f16621eeebdc8d9bc71e82c992d025
Magnus Woldrich <m@japh.se>
2011-04-22 13:08:07 +0200

    zsh: add global aliases

---
+++ zsh/02-alias-global.zsh
@@ -0,0 +1,30 @@
+#vim: tw=0 nowrap ts=2 ft=sh:
+
+alias -g     muttrc=$HOME/etc/muttrc
+alias -g procmailrc=$HOME/.procmailrc
+alias -g Xresources=$HOME/etc/Xresources
+
+
+alias -g vimhome=$HOME/etc/vim
+alias -g   vimrc=$HOME/etc/vimrc
+alias -g vimfunc=$HOME/etc/vim/functions.vim
+alias -g  vimmap=$HOME/etc/vim/mappings.vim
+alias -g   vimft=$HOME/etc/vim/filetypes.vim
+alias -g   vimab=$HOME/etc/vim/abbrevations.vim
+alias -g  vimcol=$HOME/etc/vim/colors
+
+alias -g     zshrc=$HOME/etc/zsh/zshrc
+alias -g    zshcol=$HOME/etc/zsh/01-colors.zsh
+alias -g     zshab=$HOME/etc/zsh/02-abbrevations.zsh
+alias -g  zshalias=$HOME/etc/zsh/02-alias{,-suffix,-global}.zsh
+alias -g     zshbm=$HOME/etc/zsh/02-bookmarks.zsh
+alias -g   zshcmpl=$HOME/etc/zsh/02-completion.zsh
+alias -g    zshexp=$HOME/etc/zsh/02-exports.zsh
+alias -g    zshopt=$HOME/etc/zsh/02-setopts.zsh
+alias -g   zshkeys=$HOME/etc/zsh/03-bindkeys.zsh
+alias -g   zshfunc=$HOME/etc/zsh/03-functions.zsh
+alias -g    zshgit=$HOME/etc/zsh/04-git.zsh
+alias -g    zshapp=$HOME/etc/zsh/05-appearance.zsh
+alias -g   zshperl=$HOME/etc/zsh/06-perlbrew.zsh
+alias -g zshsyntax=$HOME/etc/zsh/10-syntax.zsh
+alias -g  zshtheme=$HOME/etc/zsh/$ZSH_THEME.theme
