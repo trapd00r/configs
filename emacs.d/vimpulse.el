ae378f3aefb5c77a664d824a4d2e7c19c2f57dd4
Magnus Woldrich <m@japh.se>
2011-09-03 17:23:45 +0200

    emacs: update

---
+++ emacs.d/vimpulse.el
@@ -0,0 +1,14 @@
+(require 'vimpulse-dependencies)
+(require 'vimpulse-viper-function-redefinitions)
+(require 'vimpulse-utils)
+(require 'vimpulse-modal)
+(require 'vimpulse-ex)
+(require 'vimpulse-paren-matching)
+(require 'vimpulse-visual-mode)
+(require 'vimpulse-operator)
+(require 'vimpulse-text-object-system)
+(require 'vimpulse-misc-keybindings)
+(require 'vimpulse-compatibility)
+(provide 'vimpulse)
+
+;;; vimpulse.el ends here
--- emacs.el
+++ emacs.el
@@ -1,2 +1,13 @@
-;;(require 'vimpulse)
 (setq auto-save-default nil)
+
+(add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/evil")
+(add-to-list 'load-path "~/.emacs.d/themes")
+
+(require 'color-theme-gruber-darker)
+(require 'evil)
+(evil-mode 1)
+
+(setq color-theme-is-global t)
+
+(color-theme-gruber-darker)
