77ce8210f849303037ed5a76629652701c64e7ed
Magnus Woldrich <m@japh.se>
2011-10-23 13:19:48 +0200

    mutt: update mailpath | fix up the colors

---
+++ mutt/colors/colors.calmar-update.sh
@@ -0,0 +1,5 @@
+umask 022
+wget http://www.calmar.ws/dotfiles/dotfiledir/colors256-dark
+wget http://www.calmar.ws/dotfiles/dotfiledir/colors256-light
+mv -v colors256-dark calmar.dark
+mv -v colors256-light calmar.light
