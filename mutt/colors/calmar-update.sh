f17291f8b7550c9230a20f0e21f877f39ed26669
Magnus Woldrich <m@japh.se>
2011-10-23 13:21:27 +0200

    mutt: cleanup

--- mutt/colors/calmar-update.sh
+++
@@ -1,5 +0,0 @@
-umask 022
-wget http://www.calmar.ws/dotfiles/dotfiledir/colors256-dark
-wget http://www.calmar.ws/dotfiles/dotfiledir/colors256-light
-mv -v colors256-dark calmar.dark
-mv -v colors256-light calmar.light
