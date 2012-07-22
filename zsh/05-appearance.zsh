da43506e56dd12d34e69ef8cb19cd396eba756aa
Magnus Woldrich <m@japh.se>
2012-07-22 19:18:50 +0200

    zsh: disable everything wrt to appearance

--- zsh/05-appearance.zsh
+++ zsh/05-appearance.zsh
@@ -1,24 +1,24 @@
 # ls colors
-autoload colors; colors;
-
-setopt multios
-setopt cdablevarS
-
-if [[ x$WINDOW != x ]]
-then
-    SCREEN_NO="%B$WINDOW%b "
-else
-    SCREEN_NO=""
-fi
-
-# Apply theming defaults
-PS1="%n@%m:%~%# "
-
-# git theming default: Variables for theming the git info prompt
-#export ZSH_THEME_GIT_PROMPT_PREFIX="git->["         # Prefix at the very beginning of the prompt, before the branch name
-#export ZSH_THEME_GIT_PROMPT_SUFFIX="]"             # At the very end of the prompt
-#export ZSH_THEME_GIT_PROMPT_DIRTY="Œ"              # Text to display if the branch is dirty
-#export ZSH_THEME_GIT_PROMPT_CLEAN="afaf"               # Text to display if the branch is clean
-
-# Setup the prompt with pretty colors
-setopt prompt_subst
+#autoload colors; colors;
+#
+#setopt multios
+#setopt cdablevarS
+#
+#if [[ x$WINDOW != x ]]
+#then
+#    SCREEN_NO="%B$WINDOW%b "
+#else
+#    SCREEN_NO=""
+#fi
+#
+## Apply theming defaults
+#PS1="%n@%m:%~%# "
+#
+## git theming default: Variables for theming the git info prompt
+##export ZSH_THEME_GIT_PROMPT_PREFIX="git->["         # Prefix at the very beginning of the prompt, before the branch name
+##export ZSH_THEME_GIT_PROMPT_SUFFIX="]"             # At the very end of the prompt
+##export ZSH_THEME_GIT_PROMPT_DIRTY="Œ"              # Text to display if the branch is dirty
+##export ZSH_THEME_GIT_PROMPT_CLEAN="afaf"               # Text to display if the branch is clean
+#
+## Setup the prompt with pretty colors
+#setopt prompt_subst
