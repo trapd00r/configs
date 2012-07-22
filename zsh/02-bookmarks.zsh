f5fa1cd849bd687b005349adb4f0796b0c3c80cc
Magnus Woldrich <m@japh.se>
2012-07-22 06:00:04 +0200

    zsh: disable all bookmarks for now, and note bug trying to name a bookmark "♫"

--- zsh/02-bookmarks.zsh
+++ zsh/02-bookmarks.zsh
@@ -1,23 +1,33 @@
 #!/usr/bin/zsh
+#hash -d '♫'=${HOME}/♫
+#hash -d '⚐'=${HOME}/⚐
 
-hash -d b=~/bin
-hash -d d=~/dev
-hash -d dD=~/dev/Distributions
-hash -d dfls=~/dev/File-LsColor
-hash -d dLS=~/dev/LS_COLORS
-hash -d drpd=~/dev/RPD
-hash -d dTE=~/dev/Term-ExtendedColor
-hash -d dTET=~/dev/Term-ExtendedColor-TTY
-hash -d dTEX=~/dev/Term-ExtendedColor-Xresources
-hash -d up=~/dev/_upstream
-hash -d u=~/dev/utils
-hash -d c=~/etc
-hash -d x=~/etc
-hash -d rc=~/etc/rc.d/
-hash -d v=~/etc/vim
-hash -d h=~/http
-hash -d p=/mnt/Porn/
-hash -d pp=/mnt/Porn_2/
+#hash -d "♫"=${HOME}/.CD-current
+
+#ｑ
+#^
+#ｑ
+# ｗértyuiop/asðfgh,,;\'«»©vbnµ,..*"££$€¥{[i'¢I¢'``  
+
+#hash -d b=~/bin
+#hash -d d=~/dev
+#hash -d dD=~/dev/Distributions
+#hash -d dfls=~/dev/File-LsColor
+#hash -d dLS=~/dev/LS_COLORS
+#hash -d drpd=~/dev/RPD
+#hash -d "ｑ"=${HOME}/dev/Term-ExtendedColor
+#hash -d dTE=~/dev/Term-ExtendedColor
+#hash -d dTET=~/dev/Term-ExtendedColor-TTY
+#hash -d dTEX=~/dev/Term-ExtendedColor-Xresources
+#hash -d up=~/dev/_upstream
+#hash -d u=~/dev/utils
+#hash -d c=~/etc
+#hash -d x=~/etc
+#hash -d rc=~/etc/rc.d/
+#hash -d v=~/etc/vim
+#hash -d h=~/http
+#hash -d p=/mnt/Porn/
+#hash -d pp=/mnt/Porn_2/
 
 
 # vim: set ts=2 expandtab sw=2:
