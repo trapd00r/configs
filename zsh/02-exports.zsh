83ca288297320bab3bd6d15179560182352fbecb
Magnus Woldrich <m@japh.se>
2012-07-22 19:15:37 +0200

    zsh: reworked exports.

--- zsh/02-exports.zsh
+++ zsh/02-exports.zsh
@@ -1,81 +1,132 @@
-#!/usr/bin/zsh
+#!/bin/zsh
+### vim: ft=sh:fdm=marker:et:sw=2:
+###
+#      ${HOME}/etc/zsh/02-exports.zsh
+#   ‗‗‗‗‗‗‗‗‗‗‗‗ ‗‗‗‗‗‗ ‗‗‗‗‗‗‗‗ ‗‗‗‗‗‗‗‗‗‗‗
+#         owner  Magnus Woldrich <m@japh.se>
+#         btime  2009-04-24
+#         mtime  2012-07-22 08:28:17
+#   permissions  You are free to use things you may find useful here.
+#                Would my tweaks happen to give you a raise or fetch you a
+#                girlfriend, it goes without saying I'm counting on you
+#                to share with me as I've shared with you.
+#           git  http://github.com/trapd00r/configs/zsh  (up-to-date)
+#           url  http://devel.japh.se/configs/zsh        (sync 1/24h)
+#           irc  japh@freenode #zsh #vim #perl
+#   ‗‗‗‗‗‗‗‗‗‗‗‗ ‗‗‗‗‗‗‗‗‗‗‗‗‗ ‗‗‗‗ ‗‗‗‗ ‗‗‗‗
+###
+###
 
-if [[ $HOSTNAME == 'rambo' ]]; then
-  export DISPLAY=:0.0
+# don't worry, her scope ends at EOF
+setopt allexport
+
+[[ -f ${HOME}/lib/stderred.so ]] && LD_PRELOAD="${HOME}/lib/stderred.so"
+
+GIMP2_DIRECTORY="${HOME}/etc/gimp"
+
+#( PATH
+ BIN_STD=/bin:/usr/bin:/usr/local/bin
+BIN_PERL=/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+BIN_SUID=/sbin:/usr/sbin
+    PATH=${BIN_STD}:${BIN_HOME}:${BIN_PERL}:${BIN_SUID}
+    # not using $HOME here because UID 0 should be able to use my utils as well
+    # and "I know what I'm doing"
+    PATH=/home/scp1/bin:/home/scp1/dev/utils/:${PATH}:/home/scp1/dev/git-extras/bin
+#)
+
+
+if [[ -x =wim ]]
+then
+  EDITOR=wim
+  GIT_EDITOR="${EDITOR} -X"
+  VIDIR_EDITOR=${EDITOR}
+  VIDIR_EDITOR_ARGS='-c :set nolist | :set cc=0 | set ft=vidir-ls'
+      VIMRUNTIME=${HOME}/etc/vim
+  VIMRUNTIME_DEV=${HOME}/dev/vim/runtime
+  VIFM=${HOME}/etc/vifm
+  MYVIFMRC=$HOME/etc/vifm/vifmrc
+  PAGER=''
+  LESS=''
+else
+  perl -e 'warn "wim not found! Resort to vim...\n"';
+  EDITOR=vim
+  GIT_EDITOR="${EDITOR} -X"
+  VIDIR_EDITOR=${EDITOR}
+  VIDIR_EDITOR_ARGS='-c :set nolist | :set cc=0 | set ft=vidir-ls'
+      VIMRUNTIME=${HOME}/etc/vim
+  VIMRUNTIME_DEV=${HOME}/dev/vim/runtime
+  VIFM=${HOME}/etc/vifm
+  MYVIFMRC=$HOME/etc/vifm/vifmrc
+  PAGER=''
+  LESS=''
 fi
 
-export LD_PRELOAD="$HOME/lib/stderred.so"
+TERMINFO=${HOME}/usr/share/terminfo:/usr/share/terminfo
+DISPLAY=:0.0
+GREP_OPTIONS='-IP --color --line-buffered'
 
-export EDITOR="vim"
-export GIST_DIR="$HOME/dev/_gists/"
-export LISTMAX="300"
+HARNESS_SUMMARY_COL_FAIL="bold red"
+HARNESS_SUMMARY_COL_SUC=" bold cyan"
+PERLBREW_ROOT=${HOME}/usr/share/perl5
+PERL5LIB=${HOME}/lib
+PERL_HACK_LIB=/tmp
+PERL_MM_USE_DEFAULT=1
+
+
+ESCDELAY=10
+NCURSES_ASSUMED_COLORS=3,0
+NCURSES_NO_MAGIC_COOKIES=1
+NCURSES_NO_PADDING=1
+
+GIST_DIR="$HOME/dev/_gists/"
+PERL_CPANM_OPT="-l ${HOME} -v --interactive --mirror http://cpan.se"
+OREILLY_BOOKS_DATABASE=${HOME}/usr/share/doc/oreilly.db
+
+
+
+export LESSKEY="${HOME}/etc/lesskey"
 export MOZ_DISABLE_PANGO="1"
+export FONTCONFIG_PATH=''
 export X_OSD_COLOR='#a8ff00'
-export ACKRC="$HOME/etc/ackrc"
 export CLIVE_CONFIG="$HOME/etc/cliverc"
-export MAILDIR="/mnt/Docs/Mail/inbox/new"
+export MAILDIR="/mnt/Mail/inbox/new"
+export SENDMAIL='msmtp -C ~/.msmtprc -a gmail m@japh.se'
 export HOSTNAME="shiva"
-export BROWSER="/usr/bin/w3m"
-export PAGER="vimpager"
+export ACKRC="$HOME/etc/ackrc"
 
-# my stuff                                                                   {{{
-export OREILLY_BOOKS_DATABASE="$XDG_DOCUMENTS_DIR/oreilly.db"
-export VIDIR_EDITOR_ARGS='-c :set nolist | :set cc=0'
-#}}}
-# PATH                                                                       {{{
-export PATH="$( echo \
-  $HOME/dev/utils:\
-  ~/bin:\
-  ~/dev/_upstream/git-extras/bin:\
-  /bin:\
-  /usr/bin:\
-  /usr/local/bin:\
-  /usr/bin/site_perl:\
-  /usr/bin/core_perl:\
-  /usr/bin/vendor_perl:\
-  /usr/lib/perl5/site_perl/bin:\
-  /usr/lib/perl5/vendor_perl/bin:\
-  /usr/lib/perl5/core_perl/bin:\
-  /sbin:\
-  /usr/sbin:\
-  /usr/bin/perlbin/site:\
-  /usr/bin/perlbin/vendor:\
-  /usr/bin/perlbin/core:\
-  /home/scp1/perl5/perlbrew/perls/perl-5.8.3/:\
-  $HOME/go/bin:\
-  | perl -pe 's/\s+//g'
-)"
-export MANPATH=$MANPATH:/usr/local/man:/opt/local/share/man
-export AWKPATH=$HOME/dev/lib/awk/
-#                                                                            }}}
-# xdg specification                                                          {{{
-export XDG_CACHE_HOME="$HOME/etc/cache"
-export XDG_CONFIG_HOME="$HOME/etc"
-export XDG_DATA_HOME="$HOME/var"
-export XDG_DESKTOP_DIR="/tmp/.${UID}"
-export XDG_DOCUMENTS_DIR="${HOME}/usr/share/doc"
-export XDG_DOWNLOAD_DIR="/tmp/scp1/"
-export XDG_MUSIC_DIR="/mnt/Music_1/"
-export XDG_PICTURES_DIR="/mnt/Docs/Photo/"
-export XDG_PUBLICSHARE_DIR="/tmp/.${UID}"
-export XDG_TEMPLATES_DIR="/tmp/.${UID}"
-export XDG_VIDEOS_DIR="/mnt/Movies_1/"
-#}}}
-# terminal                                                                      {{{
-export INPUTRC='$XDG_CONFIG_HOME/inputrc'
+export BROWSER="/usr/bin/w3m"
+
+
+MANPATH=${HOME}/usr/share/man
+MANPATH=${MANPATH}:/usr/local/man
+AWKPATH=$HOME/dev/lib/awk/
+
+ XDG_CACHE_HOME="$HOME/etc/cache"
+ XDG_CONFIG_HOME="$HOME/etc"
+ XDG_DATA_HOME="$HOME/var"
+ XDG_DESKTOP_DIR="/tmp/.${UID}"
+ XDG_DOCUMENTS_DIR="${HOME}/usr/share/doc"
+ XDG_DOWNLOAD_DIR="/tmp/scp1/"
+ XDG_MUSIC_DIR="/mnt/Music_1/"
+ XDG_PICTURES_DIR="/mnt/Docs/Photo/"
+ XDG_PUBLICSHARE_DIR="/tmp/.${UID}"
+ XDG_TEMPLATES_DIR="/tmp/.${UID}"
+ XDG_VIDEOS_DIR="/mnt/Movies_1/"
+
 export RLWRAP_FILTERDIR="$HOME/dev/_upstream/rlwrap/filters"
-export WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
+
+export WORDCHARS='*?_.[]~&;!#$%^(){}<>'
 export ZSH_ACTIVE_COMPLETIONS="$( echo ${(kv)_comps[@]} )"
-export ZSH_THEME="trapd00r"
+export ZSH_THEME="ftprompt"
+export LISTMAX=0
+
 export KEYTIMEOUT="1"
-#}}}
-# history                                                                    {{{
+
 export HISTFILE=$XDG_DATA_HOME/zsh/history
 export HISTIGNORE="&:ls:[bf]g:exit:reset:clear:cd*"
 export HISTSIZE=10000
 export SAVEHIST=25000
-#}}}
-# clipbored ( https://github.com/trapd00r/clipbored )                        {{{
+
 export CLIPBORED_DMENU_FONT=${FONT}
 export CLIPBORED_DMENU_LINES=30
 export CLIPBORED_DMENU_LISTMODE='vertical'
@@ -83,63 +134,34 @@ export CLIPBORED_DMENU_NORMAL_BG='#1c1c1c'
 export CLIPBORED_DMENU_NORMAL_FG='#484848'
 export CLIPBORED_DMENU_SELECT_BG='#292929'
 export CLIPBORED_DMENU_SELECT_FG='#1c78ef'
-#                                                                            }}}
-# mpd                                                                        {{{
 export MPD_HOST='192.168.1.128'
 export MPD_PORT=6600
 export MPD_USER='scp1'
 if [ $HOST = "shiva" ]; then
   export MPD_PASS=`smokingkills`
 fi
-#}}}
-# perl                                                                       {{{
-export HARNESS_SUMMARY_COL_FAIL="bold red"
-export HARNESS_SUMMARY_COL_SUC=" bold cyan"
-export PERLBREW_ROOT="$HOME/usr/share/perl5"
-export PERL_HACK_LIB="/tmp"
-export PERL_MM_USE_DEFAULT=1
-export PERLDOC="-t"
-export VI_QUICKFIX_SOURCEFILE='stdin'
 
-export PERL5OPT="$(echo '
-  -Ilib
-  -I$HOME/dev/utils/lib
-  -MFile::Basename=basename
-  -MCarp=carp,croak,confess,cluck
-  -Mopen=:std,:utf8
-  -Mutf8
-  ' | perl -pe 's/\n//g')"
-#export RELEASE_TESTING=1
-#}}}
-# dmenu                                                                      {{{
-export DMENU_FONT=${FONT}
-export DMENU_LINES='-l 30'
-export DMENU_NORMAL_BG='#1c1c1c'
-export DMENU_NORMAL_FG='#484848'
-export DMENU_SELECT_BG='#292929'
-export DMENU_SELECT_FG='#1c78ef'
-#                                                                            }}}
-# dzen2                                                                      {{{
-export DZEN_ALIGNMENT='c'
-export DZEN_BG='#1c1c1c'
-export DZEN_FG='#ffffff'
-export DZEN_FONT=${FONT}
-export DZEN_WIDTH=1680
-export DZEN_X_POS=0
-export DZEN_Y_POS=1040
-# }}}
-# Fonts of choice                                                            {{{
-export FONT='-windows-montecarlo-medium-r-normal--0-0-72-72-c-0-microsoft-cp1252'
-export FONT_B='-windows-montecarlo-bold-r-normal--0-0-72-72-c-0-microsoft-cp1252'
-export FONT_I='-nil-profont-medium-r-normal--10-100-72-72-c-50-iso8859-1'
-#                                                                            }}}
-# Go                                                                         {{{
-export GOOS='linux'
-export GOROOT="$HOME/go"
-export GO_ARCH=386
-#}}}
-# hardcopies                                                                 {{{
-export LP_DEST="Canon_MP150"
-export PRINTER="Canon_MP150"
-#}}}
-# vim: set et sw=2 ft=sh fdm=marker:
+FONT='-windows-montecarlo-medium-r-normal--0-0-72-72-c-0-microsoft-cp1252'
+FONT_B='-windows-montecarlo-bold-r-normal--0-0-72-72-c-0-microsoft-cp1252'
+FONT_I='-nil-profont-medium-r-normal--10-100-72-72-c-50-iso8859-1'
+
+# dmenu #+
+  DMENU_FONT=${FONT}
+  DMENU_LINES='-l 30'
+  DMENU_NORMAL_BG='#1c1c1c'
+  DMENU_NORMAL_FG='#484848'
+  DMENU_SELECT_BG='#292929'
+  DMENU_SELECT_FG='#1c78ef'
+# #-
+
+DZEN_ALIGNMENT='c'
+DZEN_BG='#1c1c1c'
+DZEN_FG='#ffffff'
+DZEN_FONT=${FONT}
+DZEN_WIDTH=1680
+DZEN_X_POS=0
+DZEN_Y_POS=1040
+
+PRINTER="Canon_MP150"
+setopt allexport
+setopt no_allexport
