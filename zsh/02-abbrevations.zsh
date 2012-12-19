2bfd7cba3367208bf80f7c10c6b7a53cfc5a0bd8
Magnus Woldrich <m@japh.se>
2012-12-19 18:23:22 +0100

    zsh: add jjk => !-3$ , jkk => !-4$ abbrevations

--- zsh/02-abbrevations.zsh
+++ zsh/02-abbrevations.zsh
@@ -6,29 +6,18 @@ abbreviations=(
     perl -e "@_=<>; print sort grep {! /^[.]/ } @_; print sort grep { /^[.]/ } @_" | \
     ls_color'
 
-  'Glob'  '{*,*/*,*/*/*,*/*/*/*,*/*/*/*/*,*/*/*/*/*/*}'
-  'GlobV' '{*,*/*,*/*/*,*/*/*/*,*/*/*/*/*,*/*/*/*/*/*}.(mkv|mp4|wmv|mpe?g|avi|mov)'
-  'GlobM' '{*,*/*,*/*/*,*/*/*/*,*/*/*/*/*,*/*/*/*/*/*}.(flac|mp3)'
-  "pinc"         "/usr/{lib,share}/perl5/{core,site,vendor}_perl/"
-  "Paralel"      "Parallel"
-  "Paralell"     "Parallel"
-
-  "-MPara::Fork" "-MParallel::ForkManager"
-  "-MD::D"       "-MData::Dumper"
-  "-MT::EC"      "-MTerm::ExtendedColor=:attributes"
-  "-MT::EC::X"   "-MTerm::ExtendedColor::Xresources=get_xterm_color,set_xterm_color"
-  "-MLWP::S"     "-MLWP::Simple"
-
   "jj"         "!$"
   "jk"         "!-2$"
+  "jjk"        "!-3$"
+  "jkk"        "!-4$"
 )
 
 magic-abbrev-expand() {
-    local MATCH
-    LBUFFER=${LBUFFER%%(#m)[-:_a-zA-Z0-9]#}
-    LBUFFER+=${abbreviations[$MATCH]:-$MATCH}
-    zle self-insert
-    #printf "\b"
+  local MATCH
+  LBUFFER=${LBUFFER%%(#m)[-:_a-zA-Z0-9]#}
+  LBUFFER+=${abbreviations[$MATCH]:-$MATCH}
+  zle self-insert
+  #printf "\b"
 }
 
 no-magic-abbrev-expand() {
