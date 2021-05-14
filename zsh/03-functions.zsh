c44694586066f8102b7b24389d071afd31240e70
Magnus Woldrich <magnus.woldrich@gmail.com>
2021-05-14 07:08:42 +0200

    zsh: add ls_color to du1()

--- zsh/03-functions.zsh
+++ zsh/03-functions.zsh
@@ -40,7 +40,7 @@ tcx()       { /usr/bin/tmux attach || /usr/bin/tmux -f $XDG_CONFIG_HOME/etc/tmux
 mutt()      { TERM=xterm-256color neomutt -F $XDG_CONFIG_HOME/muttneorc -f $HOME/mail/personal/INBOX "$@" }
 tig()       { TERM=xterm-256color /usr/bin/tig "$@"}
 goto()      { [ -d "$1" ] && cd "$1" || cd "$(dirname "$1")"; }
-du1()       { du -h --max-depth=1 "$@" | sort -k 1,1hr -k 2,2f }
+du1()       { du -h --max-depth=1 "$@" | sort -k 1,1hr -k 2,2f | ls_color -k2 }
 scx()       {
 #  xcolor facebook;
 #  TERM=rxvt-unicode-256color screen -x
