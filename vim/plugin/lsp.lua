af7b4cc608ca77862c06a521ee8c21dda85ade27
Magnus Woldrich <magnus.woldrich@gmail.com>
2024-03-03 11:01:42 +0100

    vim: lsp rehaul

---
+++ vim/plugin/lsp.lua
@@ -0,0 +1,83 @@
+--require('lsp-zero')
+--
+--
+---- enable default keybindings
+--local lsp_zero = require('lsp-zero')
+--
+--lsp_zero.on_attach(function(client, bufnr)
+--  lsp_zero.default_keymaps({buffer = bufnr})
+--end)
+
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "P", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "<leader>dd", function() vim.diagnostic.goto_next() end, opts)
+--  vim.keymap.set("n", "dp", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "S", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'tsserver', 'rust_analyzer'},
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+-- this is the function that loads the extra snippets to luasnip
+-- from rafamadriz/friendly-snippets
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+    {name = 'luasnip', keyword_length = 2},
+    {name = 'buffer', keyword_length = 3},
+  },
+  formatting = lsp_zero.cmp_format(),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+})
+
+
+
+-- lua language server
+require('lspconfig').lua_ls.setup({})
+
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
--- vim/plugin/plugin_config.lua
+++ vim/plugin/plugin_config.lua
@@ -46,9 +46,63 @@ require('gitsigns').setup {
 
 -- Plugin: lualine
 
-local function hello()
-  return [[hello world]]
+local function FileLsColored()
+  -- Get the current file name
+  local filename = vim.fn.expand('%:t')
+  local extension = vim.fn.expand('%:e')
+
+  HLgroup = '%#LsColorLuaLine#'
+  -- Define colors based on file extension
+  if extension == 'sh' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=172 ctermbg=232 cterm=none')
+  elseif extension == 'lua' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=81 guifg=Blue')
+  elseif extension == 'vim' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=106 ctermbg=232 cterm=none')
+  elseif extension == 'txt' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=188 ctermbg=232 cterm=none')
+  elseif extension == 'css' or extension == 'sass' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=125 ctermbg=232 cterm=none')
+  elseif extension == 'csv' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=078 ctermbg=232 cterm=none')
+  elseif extension == 'js' or extension == 'jsp' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=074 ctermbg=232 cterm=none')
+  elseif extension == 'json' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=178 ctermbg=232 cterm=none')
+  elseif extension == 'log' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=190 ctermbg=232 cterm=none')
+  elseif extension == 'markdown' or extension == 'md' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=190 ctermbg=232 cterm=bold')
+  elseif extension == 'README' or extension == 'CHANGES' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=190 ctermbg=232 cterm=bold')
+  elseif extension == 'php' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=081 ctermbg=232 cterm=none')
+  elseif extension == 'Makefile' or string.match(extension, '^Makefile%.PL?$') then
+    vim.cmd('highlight LsColorLuaLine ctermfg=196 ctermbg=232 cterm=none')
+  elseif extension == 'pl' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=208 ctermbg=232 cterm=none')
+  elseif extension == 'pm' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=203 ctermbg=232 cterm=none')
+  elseif extension == 'sql' or extension == 'sqlite' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=222 ctermbg=232 cterm=none')
+  elseif extension == 't' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=114 ctermbg=232 cterm=none')
+  elseif extension == 'tt' then
+    vim.cmd('highlight LsColorLuaLine ctermfg=173 ctermbg=232 cterm=none')
+  elseif string.match(extension, 'html?$') then
+    vim.cmd('highlight LsColorLuaLine ctermfg=132 ctermbg=232 cterm=none')
+  elseif string.match(extension, 'php?$') then
+    vim.cmd('highlight LsColorLuaLine ctermfg=081 ctermbg=232 cterm=none')
+  else
+    HLgroup = '%#Normal#' -- Set to Normal highlight group for other extensions
+  end
+
+
+
+  -- Return the colored filename
+  return HLgroup .. filename .. '%#Normal#'
 end
+
 
 require('lualine').setup {
   options = {
@@ -70,12 +124,12 @@ require('lualine').setup {
     }
   },
   sections = {
---    lualine_a = {'mode'},
---    lualine_a = {hello},
+    lualine_a = {'mode'},
+    lualine_b = {FileLsColored},
 --    lualine_a = {'searchcount'},
 --    lualine_a = {'FileNameStatusLine'},
-    lualine_a = {'filename'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+--    lualine_a = {'filename'},
+    lualine_c = {'branch', 'diff', 'diagnostics'},
 --    lualine_c = {'filename'},
  --   lualine_c = {'FileNameStatusLine'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
@@ -132,125 +186,3 @@ require('winbar').setup({
         'qf',
     }
 })
-
--- Completion
--- Set up nvim-cmp.
-local cmp = require'cmp'
-
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
-
-
--- Setup language servers.
------
-local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {}
-lspconfig.tsserver.setup {}
-lspconfig.rust_analyzer.setup {
-  -- Server-specific settings. See `:help lspconfig-setup`
-  settings = {
-    ['rust-analyzer'] = {},
-  },
-}
-
-
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'P', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
-})
-
--- php LSP 
-require'lspconfig'.intelephense.setup{}
-
-
--- Plugin: mason
-require("mason").setup()
