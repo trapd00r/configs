2ea92ff249c9e67db27a9880ad6b4942a7bd814e
Magnus Woldrich <magnus.woldrich@gmail.com>
2024-03-08 08:13:08 +0100

    vim: enable treesitter

--- vim/plugin/lsp.lua
+++ vim/plugin/lsp.lua
@@ -44,24 +44,24 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
-require('luasnip.loaders.from_vscode').lazy_load()
-
-cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<Enter>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-})
+--require('luasnip.loaders.from_vscode').lazy_load()
+--
+--cmp.setup({
+--  sources = {
+--    {name = 'path'},
+--    {name = 'nvim_lsp'},
+--    {name = 'nvim_lua'},
+--    {name = 'luasnip', keyword_length = 2},
+--    {name = 'buffer', keyword_length = 3},
+--  },
+--  formatting = lsp_zero.cmp_format(),
+--  mapping = cmp.mapping.preset.insert({
+--    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+--    ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+--    ['<C-Space>'] = cmp.mapping.complete(),
+--  }),
+--})
 
 
 
