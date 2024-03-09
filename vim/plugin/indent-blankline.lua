50989f9a7f8ae4999ab9a64a895aa7a514bedcf9
Magnus Woldrich <magnus.woldrich@gmail.com>
2024-03-09 11:08:11 +0100

    vim: update

---
+++ vim/plugin/indent-blankline.lua
@@ -0,0 +1,15 @@
+-- local highlight = {
+--     "CursorColumn",
+--     "Whitespace",
+-- }
+-- require("ibl").setup {
+--     indent = { highlight = highlight, char = "" },
+--     whitespace = {
+--         highlight = highlight,
+--         remove_blankline_trail = false,
+--     },
+--     scope = { enabled = false },
+-- }
+--
+
+require("ibl").setup()
