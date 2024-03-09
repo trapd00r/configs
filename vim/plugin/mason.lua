2fe5755b9af9c650fcc52f4e551508a60385ef55
Magnus Woldrich <magnus.woldrich@gmail.com>
2024-03-09 10:52:55 +0100

    update

---
+++ vim/plugin/mason.lua
@@ -0,0 +1,12 @@
+
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "[Y]",
+            package_pending = "[P]",
+            package_uninstalled = "[N]"
+        },
+    border = "double",
+
+    }
+})
