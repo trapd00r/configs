4c35c10a25f95253d8eeedb03bf92ea1d3eddfd3
Magnus Woldrich <magnus.woldrich@gmail.com>
2024-03-10 15:03:28 +0100

    vim: add gq using lsp

--- vim/plugin/haxx.lua
+++ vim/plugin/haxx.lua
@@ -1,7 +1,7 @@
 -- fix bug https://github.com/neovim/neovim/issues/21856
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
   callback = function()
---    vim.fn.jobstart('notify-send "lol"', {detach=true})
+    --    vim.fn.jobstart('notify-send "lol"', {detach=true})
   end,
 })
 
@@ -10,11 +10,11 @@ vim.g.diagnostics_active = true
 function _G.toggle_diagnostics()
   if vim.g.diagnostics_active then
     vim.g.diagnostics_active = false
---    vim.lsp.diagnostic.clear(0)
+    --    vim.lsp.diagnostic.clear(0)
     vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
   else
-     vim.g.diagnostics_active = true
-     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.g.diagnostics_active = true
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
       vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = true,
         signs = true,
@@ -28,5 +28,20 @@ end
 vim.api.nvim_set_keymap('n', '<leader>DD', ':call v:lua.toggle_diagnostics()<CR>', { noremap = true, silent = true })
 
 
--- gq using lsp
+-- gq using lsp, single line
+local function format_current_line()
+  -- Get the current buffer number
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  -- Get the current line number
+  local line = vim.fn.line(".")
+
+  -- Format the current line using vim.lsp.buf.format
+  vim.lsp.buf.format({ bufnr, bufnr }, { line, line })
+end
+
+-- Set the custom key mapping for 'gq' to trigger the formatting function
+vim.keymap.set("n", "gq", format_current_line, { noremap = true })
+
+-- gq using lsp, visual selection
 vim.keymap.set("v", "gq", vim.lsp.buf.format, { remap = false })
