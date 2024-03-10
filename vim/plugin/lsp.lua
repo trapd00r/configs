6fd78360f6b0fea19b6d9f9d53c99ac48dad14fa
Magnus Woldrich <magnus.woldrich@gmail.com>
2024-03-10 10:39:09 +0100

    update

--- vim/plugin/lsp.lua
+++ vim/plugin/lsp.lua
@@ -49,6 +49,37 @@ require('mason-lspconfig').setup({
   },
 })
 
+require'lspconfig'.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        -- library = {
+        --   vim.env.VIMRUNTIME
+        --   -- Depending on the usage, you might want to add additional paths here.
+        --   -- "${3rd}/luv/library"
+        --   -- "${3rd}/busted/library",
+        -- }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+}
 
 
 -- Borders for popups
