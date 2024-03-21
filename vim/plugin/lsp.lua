b5db77b3cf604ddbdbb3fccd06544dbd082078a8
Magnus Woldrich <magnus.woldrich@gmail.com>
2024-03-21 07:50:22 +0100

    vim: enable ctags again (for magento)

--- vim/plugin/lsp.lua
+++ vim/plugin/lsp.lua
@@ -25,6 +25,10 @@ local _default_opts = win.default_opts
 
 require('lspconfig.ui.windows').default_options.border = 'double'
 
+local cmp_lsp = require('cmp_nvim_lsp')
+local capabilities = cmp_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+
 
 
 lsp_zero.on_attach(function(client, bufnr)
@@ -158,6 +162,21 @@ require('lspconfig').html.setup({})
 --   }
 -- })
 
+require('lspconfig').phpactor.setup({
+  capabilities = capabilities, -- from the local capabilities variable in the above snippet
+  settings = {
+    phpactor = {
+      enable = true,
+      completion = {
+        fullyQualifyGlobalConstantsAndFunctions = false
+      },
+      phpdoc = {
+        returnVoid = false,
+      }
+    },
+  }
+})
+
 
 
 lsp_zero.set_sign_icons({
@@ -217,3 +236,20 @@ require'lspconfig'.perlnavigator.setup{
       }
     }
 }
+
+vim.keymap.set('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n','P','<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+vim.keymap.set('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+vim.keymap.set('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+vim.keymap.set('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+vim.keymap.set('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+vim.keymap.set('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+vim.keymap.set('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
