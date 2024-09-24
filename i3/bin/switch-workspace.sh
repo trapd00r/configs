b4f9625bbe56278fb63c4cac25d3248bb895526e
Magnus Woldrich <magnus.woldrich@gmail.com>
2024-09-24 20:35:30 +0200

    update i3

--- i3/bin/switch-workspace.sh
+++ i3/bin/switch-workspace.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# switch workspaces properly on different outputs
+# use mod+1, mod+2 etc to switch to workspaces on different monitors
+# there is 5 workspaces on the main monitor, but on the other ones there is only 2
+# workspace 6,7 on left monitor is treated like workspace 1,2 so
+# we can use the same keybindings to switch to them
+# in practice, emulating a more sane workspace setup (ratpoison, sdorfehs)
 
 # Get the name of the active output
 focused_output=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).output')
@@ -34,3 +38,41 @@ case "$focused_output" in
         ;;
 esac
 
+# i3 config:
+
+# # define outputs
+# set $middle DP-2
+# set $left DP-0
+# set $top DP-5
+# set $behind HDMI-0
+#
+# set $ws1 1
+# set $ws2 2
+# set $ws3 3
+# set $ws4 4
+# set $ws5 5
+# set $ws6 6
+# set $ws7 7
+# set $ws8 8
+# set $ws9 9
+#
+# # lock workspaces to outputs
+# workspace $ws1 output $middle
+# workspace $ws2 output $middle
+# workspace $ws3 output $middle
+# workspace $ws4 output $middle
+# workspace $ws5 output $middle
+# workspace $ws6 output $left
+# workspace $ws7 output $left
+# workspace $ws8 output $top
+# workspace $ws9 output $top
+#
+#
+# # workspace 1 on different monitors
+# # in reality we switch to workspace 6,7 etc
+# bindsym $mod+1 exec --no-startup-id /bin/bash ~/etc/i3/bin/switch-workspace.sh 1
+# bindsym $mod+2 exec --no-startup-id /bin/bash ~/etc/i3/bin/switch-workspace.sh 2
+#
+# bindsym $mod+3 workspace number $ws3
+# bindsym $mod+4 workspace number $ws4
+# bindsym $mod+5 workspace number $ws5
