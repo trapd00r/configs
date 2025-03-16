4efd46bbe95224be5738aaa5eb24dd6cf1759e1b
Magnus Woldrich <magnus.woldrich@gmail.com>
2025-03-16 08:42:51 +0100

    vim: some cleanup

--- vim/plugin/notify.lua
+++ vim/plugin/notify.lua
@@ -1,18 +1,18 @@
-require("notify").setup({
-    -- background_colour = "NotifyBackground",
-    -- fps = 30,
-    icons = {
-        DEBUG = "D",
-        ERROR = "E",
-        INFO = "I",
-        TRACE = "T",
-        WARN = "W"
-    },
-    level = 2,
-    minimum_width = 50,
-    render = "default",
-    stages = "static",
-    timeout = 5000,
-    top_down = true
-
-})
+-- require("notify").setup({
+--     -- background_colour = "NotifyBackground",
+--     -- fps = 30,
+--     icons = {
+--         DEBUG = "D",
+--         ERROR = "E",
+--         INFO = "I",
+--         TRACE = "T",
+--         WARN = "W"
+--     },
+--     level = 2,
+--     minimum_width = 50,
+--     render = "default",
+--     stages = "static",
+--     timeout = 5000,
+--     top_down = true
+--
+-- })
